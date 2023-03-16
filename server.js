560eb7130468dd3f730807d66e3764cef77fbbdf
Vlad Balas <balash_vlad@yahoo.com>
2023-03-16 20:18:38 +0000

    Getting entries

--- server.js
+++ server.js
@@ -112,26 +112,19 @@ app.get('/profile/:id', (req, res) => {
 			}
 		})
 		.catch(err => res.status(400).json('error getting user'))
-	// if (!found) {
-	// 	res.status(400).json('not found');
-	// }
 })
 
 
 // IMAGE ROUTE
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'))
 })
 
 

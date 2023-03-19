2f6732a8d882a4fd9eaeb921581a0fcba4137f3d
Vlad Balas <balash_vlad@yahoo.com>
2023-03-19 16:48:05 +0000

    Updated the listening PORT

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 // SERVER LISTENING
-app.listen(3000, () => { console.log('app is running on port 3000') })
+app.listen(process.env.PORT || 3000, () => { console.log(`App is running on port ${process.env.PORT}`) })
